f3a4ab21141c496b4d00bed0d0fc16dd837af280
Alfredo Sánchez <kalamastra@K8A.local>
2026-03-05 20:14:16 -0600

    fix: remove ecr output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,11 +3,6 @@ output "api_url" {
   value       = "https://${aws_lb.main.dns_name}"
 }
 
-output "ecr_repository_url" {
-  description = "ECR repository URL"
-  value       = aws_ecr_repository.api.repository_url
-}
-
 output "ecs_cluster_name" {
   description = "ECS cluster name"
   value       = aws_ecs_cluster.main.name
